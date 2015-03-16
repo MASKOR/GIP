694460185ea01b3777bb526ee4ca39659a0ce664
merbach <erbach@fh-aachen.de>
2015-03-16 13:02:14 +0100

    Update hello_world.cpp

--- src/hello_world.cpp
+++ src/hello_world.cpp
@@ -6,5 +6,6 @@ int main(int argc, char* argv[]) {
   std::cout << "Hello World!" << std::endl;
 
   std::cout << "UCT is awesome!!!" << std::endl;
+  std::cout << "No. Batman is awesome!!!" << std::endl;
   return 0;
 }
